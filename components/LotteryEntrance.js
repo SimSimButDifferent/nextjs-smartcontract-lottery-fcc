60ce944e6a19dfaeca09c102eabf05991df68436
simsim <simeon.campbell@live.com>
2022-10-19 15:21:47 +0700

    Button and notifications

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,79 @@
 import { useWeb3Contract } from "react-moralis"
+import { contractAddresses, abi } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
-    // const {runContractFunction: enterLottery} = useWeb3Contract({
-    //     abi: ,
-    //     contractAddress: ,
-    //     functionName: ,
-    //     params: {},
-    //     msgValue:
-    // })
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0")
 
-    return <div>Hi from LotteryEntrance</div>
+    const dispatch = useNotification()
+
+    const { runContractFunction: enterLottery } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "enterLottery",
+        params: {},
+        msgValue: entranceFee,
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const entranceFeeFromCall = (await getEntranceFee()).toString()
+                setEntranceFee(entranceFeeFromCall)
+                console.log(entranceFee)
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div>
+            Hi from Lottery Entrance!
+            {lotteryAddress ? (
+                <div>
+                    <button
+                        onClick={async function () {
+                            await enterLottery({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
+                        }}
+                    >
+                        Enter Lottery
+                    </button>
+                    Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
+                </div>
+            ) : (
+                <div>No Lottery Address Detected</div>
+            )}
+        </div>
+    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
-            <Component {...pageProps} />
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
         </MoralisProvider>
     )
 }
