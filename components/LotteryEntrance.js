4cbeb4cdf7aee9226994534571ab1ab50de372df
simsim <simeon.campbell@live.com>
2022-10-23 08:38:16 +0700

    UpdateUi bug fix

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,14 +1,17 @@
-import { useWeb3Contract } from "react-moralis"
+import { useMoralis, useWeb3Contract } from "react-moralis"
 import { contractAddresses, abi } from "../constants"
-import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
 import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
-    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled, Moralis } = useMoralis()
+    // Re-rendered everytime due to connect button
     const chainId = parseInt(chainIdHex)
+
     const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    // State hooks
     const [entranceFee, setEntranceFee] = useState("0")
     const [recentWinner, setRecentWinner] = useState("0")
     const [numPlayers, setNumPlayers] = useState("0")
@@ -17,6 +20,7 @@ export default function LotteryEntrance() {
 
     const {
         runContractFunction: enterLottery,
+        data: enterTxResponse,
         isLoading,
         isFetching,
     } = useWeb3Contract({
@@ -26,6 +30,8 @@ export default function LotteryEntrance() {
         params: {},
         msgValue: entranceFee,
     })
+
+    // View functions
 
     const { runContractFunction: getEntranceFee } = useWeb3Contract({
         abi: abi,
@@ -59,16 +65,9 @@ export default function LotteryEntrance() {
 
     useEffect(() => {
         if (isWeb3Enabled) {
-            async function updateUI() {}
             updateUI()
         }
     }, [isWeb3Enabled])
-
-    const handleSuccess = async function (tx) {
-        await tx.wait(1)
-        handleNewNotification(tx)
-        updateUI()
-    }
 
     const handleNewNotification = function () {
         dispatch({
@@ -80,33 +79,45 @@ export default function LotteryEntrance() {
         })
     }
 
+    const handleSuccess = async (tx) => {
+        try {
+            await tx.wait(1)
+            updateUI()
+            handleNewNotification(tx)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="p-5">
-            Hi from Lottery Entrance!
+            <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
             {lotteryAddress ? (
-                <div className="">
+                <>
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
-                        onClick={async function () {
+                        onClick={async () =>
                             await enterLottery({
+                                // onComplete:
+                                // onError:
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
-                        }}
+                        }
                         disabled={isLoading || isFetching}
                     >
                         {isLoading || isFetching ? (
                             <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
                         ) : (
-                            <div>Enter Lottery</div>
+                            "Enter Lottery"
                         )}
                     </button>
                     <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
-                    <div>Number of players: {numPlayers}</div>
-                    <div>Recent Winner: {recentWinner}</div>
-                </div>
+                    <div>The current number of players is: {numPlayers}</div>
+                    <div>The most previous winner was: {recentWinner}</div>
+                </>
             ) : (
-                <div>No Lottery Address Detected</div>
+                <div>Please connect to a supported chain </div>
             )}
         </div>
     )
